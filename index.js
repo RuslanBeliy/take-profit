148379cfa5f15997731f45572087d7ba3e6540d2
Ruslan Belyi <sinkara555@gmail.com>
2025-01-19 17:03:12 +0300

    update

--- index.js
+++ index.js
@@ -1,58 +1,96 @@
-const takes = document.getElementById('list');
-const addButton = document.querySelector('.add');
-const resetButton = document.querySelector('.reset');
-const calculateButton = document.querySelector('.calculate');
+const resultBox = document.querySelector('.result');
+const formBtns = document.querySelector('.form-btns');
+const form = document.querySelector('.form');
+const takes = document.querySelector('.form-takes');
+const bankInput = document.querySelector('.bank-input');
 
-let countTakes = 1;
-let result = 0;
+bankInput.value = localStorage.getItem('bank') ?? '';
 
-const addTake = () => {
-  countTakes += 1;
-  takes.insertAdjacentHTML(
-    'beforeend',
-    `<li>
-          <h2>${countTakes} тейк</h2>
-
-          <div class="inputs">
-            <label>
-              Процент
-              <input type="number" />
-            </label>
-            <label>
-              Цена
-              <input type="number" />
-              </label>
-          </div>
-        </li>`
-  );
+const result = {
+  rr: 0,
+  take: 0,
+  volume: 0,
 };
 
 const setResult = () => {
-  const wrap = document.querySelector('.wrap');
-  if (wrap.lastElementChild.tagName === 'H2') wrap.lastElementChild.remove();
-  wrap.insertAdjacentHTML('beforeend', `<h2>Результат: ${result}</h2>`);
+  resultBox.textContent = '';
+
+  resultBox.insertAdjacentHTML(
+    'afterbegin',
+    `<div class="result-item">R/R: <strong>${result.rr}</strong></div>
+      <div class="result-item">Объем сделки: <strong>${result.volume}</strong></div>
+      <div class="result-item">Средний тейк: <strong>${result.take}</strong></div>`
+  );
+};
+
+const calculate = () => {
+  const dataForm = Array.from(new FormData(form));
+  result.rr = 0;
+  result.take = 0;
+  result.volume = 0;
+
+  let percent = 0;
+  let risk = 0;
+  let bank = 0;
+  let buyPrice = 0;
+  let stopLoss = 0;
+
+  for (const el of dataForm) {
+    const key = el[0];
+    const value = +el[1];
+
+    if (key.includes('percent-')) percent = value;
+    if (key.includes('price-')) result.take += (percent / 100) * value;
+    if (key.includes('buy')) buyPrice = value;
+    if (key.includes('stop')) stopLoss = value;
+    if (key.includes('risk')) risk = value;
+    if (key.includes('bank')) bank = value;
+
+    result.volume = ((risk / 100) * bank) / (buyPrice - stopLoss);
+    result.rr = ((result.take - buyPrice) / (buyPrice - stopLoss)).toFixed(2);
+  }
+
+  setResult();
+};
+
+const addTake = () => {
+  takes.insertAdjacentHTML(
+    'beforeend',
+    `<div class="form-take">
+            <h2>${takes.children.length + 1} тейк</h2>
+
+            <div class="form-take-inputs">
+              <label>
+                Процент
+                <input type="number" name="take-percent-${takes.children.length + 1}" step="any" />
+              </label>
+              <label>
+                Цена
+                <input type="number" name="take-price-${takes.children.length + 1}" step="any" />
+              </label>
+            </div>`
+  );
 };
 
 const reset = () => {
   takes.textContent = '';
-  countTakes = 0;
-  result = 0;
-  setResult();
+  resultBox.textContent = '';
+
+  form.querySelector('[name="buy-price"]').value = '';
+  form.querySelector('[name="stop-loss"]').value = '';
+
+  addTake();
 };
 
-const calculate = () => {
-  result = 0;
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-  document.querySelectorAll('.inputs').forEach((el) => {
-    const percent = +el.firstElementChild.children[0].value;
-    const price = +el.lastElementChild.children[0].value;
-
-    result = (percent / 100) * price + result;
-  });
-
-  setResult();
-};
-
-addButton.addEventListener('click', addTake);
-resetButton.addEventListener('click', reset);
-calculateButton.addEventListener('click', calculate);
+  calculate();
+});
+formBtns.addEventListener('click', (e) => {
+  if (e.target.classList.contains('add')) addTake();
+  if (e.target.classList.contains('reset')) reset();
+});
+bankInput.addEventListener('change', (e) => {
+  localStorage.setItem('bank', e.target.value);
+});
